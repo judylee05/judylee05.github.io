77c84fe00c7f2ccc87f329a385a015087f7c86f9
judylee05 <55869220+judylee05@users.noreply.github.com>
2020-11-24 20:02:07 -0800

    test 1
    testing github pages with 1st commit

---
+++ project.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", increaseTextBox);
+
+
+function increaseTextBox(){
+    // increase the size of the message text box in portfolio.html
+    textBox = document.getElementById('messageInput')
+
+    // increase the height and width
+    textBox.style.width="1000px";
+}
+
+// code below is for the carousel on the home page (index.html)
+// referenced Matthew Croak's code at https://medium.com/better-programming/make-a-slideshow-with-automatic-and-manual-controls-using-html-css-and-javascript-b7e9305168f9
+
+var slideIndex = 1;
+
+var myTimer;
+
+var slideshowContainer;
+
+window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 4000);
+
+    slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
+
+    slideshowContainer.addEventListener('mouseenter', pause)
+    slideshowContainer.addEventListener('mouseleave', resume)
+})
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides(n){
+    clearInterval(myTimer);
+    if (n < 0){
+        showSlides(slideIndex -= 1);
+    } else {
+        showSlides(slideIndex += 1);
+    }
+
+    if (n === -1){
+        myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
+    } else {
+        myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+    }
+}
+
+//Controls the current slide and resets interval if needed
+function currentSlide(n){
+    clearInterval(myTimer);
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+pause = () => {
+    clearInterval(myTimer);
+}
+
+resume = () =>{
+    clearInterval(myTimer);
+    myTimer = setInterval(function(){plusSlides(slideIndex)}, 4000);
+}
+
+increaseTextBox()
